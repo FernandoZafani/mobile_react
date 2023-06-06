3d369e81ed6c68ff687532f7a7f43b44276934f3
Fernando Zafani <zafani07@gmail.com>
2023-06-06 20:47:41 -0300

    UniZafs
    estude

---
+++ UniZafs/components/RenderItens.js
@@ -0,0 +1,27 @@
+
+import { StyleSheet, Text, View, Pressable, Image} from 'react-native';
+
+export const RenderItens = (props) => {
+
+    return    <View style={ styles.item }>
+                <Text style={{ marginLeft: 10 }}>{props.universidade} -- {props.pais}</Text>
+              </View>
+           
+  }
+
+  const styles = StyleSheet.create({
+    item: {
+        flexDirection: "row", 
+        alignItems: "center",
+        flex: 1,
+        borderRadius:8, 
+        borderColor: "black",
+        borderWidth: 1,
+        color:"white", 
+        margin: 10, 
+        padding: 10, 
+        backgroundColor: "pink"
+      },
+  })
+
+
